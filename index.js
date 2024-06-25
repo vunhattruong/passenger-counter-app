196e08f50895cbcd4948b71a63c5a59c481abc12
truongvu <truong.vun1@homecredit.vn>
2024-06-25 00:04:20 -0700

    [JS][First] Add save button and counting string

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 let count = 0;
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 function increment() {
   count = count + 1;
   countEl.innerText = count;
+}
+
+function save() {
+  let saveString = count + " - ";
+  saveEl.textContent += saveString;
+  countEl.textContent = 0;
+  count = 0;
 }
